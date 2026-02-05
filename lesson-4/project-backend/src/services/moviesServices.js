4c3987c0d9263d2dcb40815ecfab71ae8d0f42b8
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2026-02-05 19:47:58 +0100

    finish lesson 4

---
+++ lesson-4/project-backend/src/services/moviesServices.js
@@ -0,0 +1,47 @@
+import {readFile, writeFile} from "node:fs/promises";
+import {resolve} from "node:path";
+import { nanoid } from "nanoid";
+
+const moviesPath = resolve("src", "db", "movies.json");
+
+const updateMovies = movies => writeFile(moviesPath, JSON.stringify(movies, null, 2));;
+
+export const getMovies = async ()=> {
+    const data = await readFile(moviesPath, "utf-8");
+    return JSON.parse(data);
+}
+
+export const getMovieById = async id => {
+    const movies = await getMovies();
+    const result = movies.find(item => item.id === id);
+    return result || null;
+}
+
+export const addMovie = async data => {
+    const movies = await getMovies();
+    const newMovie = {
+        id: nanoid(),
+        ...data,
+    };
+    movies.push(newMovie);
+    await updateMovies(movies);
+    return newMovie;
+}
+
+export const updateMovieById = async (id, data)=> {
+    const movies = await getMovies();
+    const idx = movies.findIndex(item => item.id === id);
+    if(idx === -1) return null;
+    movies[idx] = {...movies[idx], ...data};
+    await updateMovies(movies);
+    return movies[idx];
+}
+
+export const deleteMovieById = async id => {
+    const movies = await getMovies();
+    const idx = movies.findIndex(item => item.id === id);
+    if(idx === -1) return null;
+    const [result] = movies.splice(idx, 1);
+    await updateMovies(movies);
+    return result;
+}
